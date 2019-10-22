1f1ec7ed7ab3993a511fad95ecf13c49ee0d0e79
cooperhurt <40371983+cooperhurt@users.noreply.github.com>
2019-10-21 19:24:28 -0600

    Fixed

---
+++ SlapJack/SlapJack/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlapJack
+{
+    public class Deck : Card
+    {
+        public List<Card> cards = new List<Card>();
+
+        public static Queue<Card> CreateDeck()
+        {
+            Queue<Card> deck = new Queue<Card>();
+            foreach (CardNumber num in Enum.GetValues(typeof(CardNumber)))
+            {
+                foreach (Suits suit in Enum.GetValues(typeof(Suits)))
+                {
+                    deck.Enqueue(new Card()
+                    {
+                        Suits = suit,
+                        CardNumber = num,
+                        image = "img/" + suit.ToString() + "/" + num.ToString(),
+                    });
+                }
+            }
+
+            return deck;
+        }
+    }
+}
---
+++ SlapJack/SlapJack/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlapJack
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public Deck Hand { get; set; }
+    }
+}
