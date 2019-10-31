c7184bc29342aaf4fa408cda37b5e1531092da9a
ZmanElete <zacharywright1@mail.weber.edu>
2019-10-30 20:09:57 -0600

    Garbage

---
+++ SlapJack/SlapJack/CurrentDeck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SlapJack.Pages;
+
+namespace SlapJack
+{
+    static public class CurrentDeck
+    {
+        public static Deck deck = new Deck();
+    }
+}
---
+++ SlapJack/SlapJack/Pages/Hub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlapJack.Pages
+{
+    public class MyHub : Hub
+    {
+
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Connection {Context.ConnectionId} made!");
+            await base.OnConnectedAsync();
+        }
+
+
+
+        // Optionally override what happens when a client disconnects
+
+        public override async Task OnDisconnectedAsync(Exception execption)
+        {
+            Console.WriteLine($"{Context.ConnectionId} disconnected!");
+
+            await base.OnDisconnectedAsync(execption);
+
+        }
+    }
+}
