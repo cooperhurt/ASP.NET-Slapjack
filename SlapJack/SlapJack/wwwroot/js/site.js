856f030112889e5232d9bcaa8b06e46700e64747
cooperhurt <40371983+cooperhurt@users.noreply.github.com>
2019-10-30 20:15:39 -0600

    Added event listener and added hub

--- SlapJack/SlapJack/wwwroot/js/site.js
+++ SlapJack/SlapJack/wwwroot/js/site.js
@@ -15,3 +15,15 @@ if (pageName == '/') {
         document.getElementById("rules").style.display = "inline";
     }
 }
+
+document.getElementById("slap").addEventListener("click", slapDeck);
+document.getElementById("slap").addEventListener("click", playCard);
+
+
+function slapDeck() {
+    alert("you slapped the deck");
+}
+
+function playCard() {
+    alert("You played a card");
+}
---
+++ SlapJack/SlapJack/wwwroot/lib/signalr/Hubs/GameHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace SlapJack.wwwroot.lib.signalr.Hubs
+{
+    public class GameHub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
