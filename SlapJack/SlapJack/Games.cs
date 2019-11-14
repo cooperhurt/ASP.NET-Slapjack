19a30eee6d17c2656afc3d486996379a93acd9a0
ZmanElete <zacharywright1@mail.weber.edu>
2019-11-13 20:40:33 -0700

    Back to the game being static

---
+++ SlapJack/SlapJack/Games.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlapJack
+{
+    public static class Games
+    {
+        public static Dictionary<int, Game> games = new Dictionary<int, Game>();
+    }
+}
